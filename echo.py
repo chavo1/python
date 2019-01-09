15fc893b806730e19649e4e551e580b21cc59228
chavo1 <chavdar.rakov@gmail.com>
2019-01-09 17:15:35 +0200

    init

---
+++ echo.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+print("__name__ is", __name__)
---
+++ import_echo.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+import echo
+print("After import, __name__ is", __name__,
+"and echo.__name__ is", echo.__name__)
