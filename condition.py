13ef606fc78146d73b6d5ce00221d12bfba29de1
chavo1 <chavdar.rakov@gmail.com>
2019-01-15 17:01:53 +0200

    ch9-1

---
+++ condition.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Conditional statement important 
+
+print("If you type < Arya Stark > will print Valar Morghulis: ")
+name = input()
+
+if name == "Arya Stark":
+    print("Valar Morghulis")
+elif name == "John Snow":
+    print("You Know Nothing")
+else:
+    print("Carry on")
