5056d93aa7378ba066e0a5c57f1e84552080e74c
chavo1 <chavdar.rakov@gmail.com>
2019-02-22 09:42:25 +0200

    ch13-3

--- linear_search_2.py
+++ linear_search_2.py
@@ -1,8 +1,18 @@
+#!/usr/bin/env python3
+
+# for Loop Version of Linear Search
 def linear_search(lst, value):
-	"""
-	return the index of the first occurence of value in lst
-	>>> linear_search([2,5,1,-3],5)
-	"""
+	"""Return the index of the first occurrence of value in lst, or return
+	 -1 if value is not in lst.
+    >>> linear_search([2, 5, 1, -3], 5)
+    1
+    >>> linear_search([2, 4, 2], 2)
+    0
+    >>> linear_search([2, 5, 1, -3], 4)
+	-1
+    >>> linear_search([], 5)
+    -1
+    """
 	for i in range(len(lst)):
 		if lst[i] == value:
 			return i
--- linear_search_3.py
+++ linear_search_3.py
@@ -1,17 +1,20 @@
+#!/usr/bin/env python3
+
+# Sentinel Search
 def linear_search(lst, value):
 	"""
-	return the index of the first occurence of value in lst
+	return the index of the first occurrence of value in lst
 	>>> linear_search([2,5,1,-3],5)
 	"""
-
+	# Add the sentinel.
 	lst.append(value)
 	i= 0
-
+	# Keep going until we find value.
 	while lst[i] != value:
-		i += 1
-
+		i = i + 1
+	# Remove the sentinel.
 	lst.pop()
-
+	# If we reached the end of the list we didn't find value.
 	if i == len(lst):
 		return -1
 	else:
